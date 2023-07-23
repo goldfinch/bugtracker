ed3c8ba5ab3de99cb188661fa4f7397cde091dac
Art Karasev <as.karasyov@gmail.com>
2023-07-23 22:58:32 +1200

    add request handler extension

---
+++ src/Extensions/BugHandlerExtension.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Goldfinch\BugTracker\Extensions;
+
+use Goldfinch\BugTracker\Models\Bug;
+use SilverStripe\Core\Extension;
+use SilverStripe\Control\HTTPRequest;
+use SilverStripe\Security\SecurityToken;
+
+class BugHandlerExtension extends Extension
+{
+    public function onBeforeHTTPError($statusCode, HTTPRequest $request, $errorMessage = null)
+    {
+        $data = [
+          'url' => $request->getUrl(),
+          'referer' => $request->getHeader('referer'),
+          'extension' => $request->getExtension(),
+          'sec-fetch-dest' => $request->getHeader('sec-fetch-dest'),
+          'accept' => $request->getHeader('accept'),
+          'host' => $request->getHeader('host'),
+          'scheme' => $request->getScheme(),
+          'method' => $request->httpMethod(),
+          'ip' => $request->getIp(),
+          'route' => $request->routeParams(),
+          'security-id' => SecurityToken::getSecurityID(),
+        ];
+
+        $bug = new Bug;
+        $bug->Data = json_encode($data);
+        $bug->write();
+    }
+}
