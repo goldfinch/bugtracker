13606499d3055b0190b1ee71a869dfe5e5288e1c
Art Karasev <as.karasyov@gmail.com>
2023-12-14 12:23:30 +1300

    json twean

--- src/Models/Bug.php
+++ src/Models/Bug.php
@@ -4,7 +4,7 @@ namespace Goldfinch\BugTracker\Models;
 
 use SilverStripe\ORM\DataObject;
 use SilverStripe\Security\Permission;
-use Goldfinch\JSONEditor\ORM\FieldType\DBJSONText;
+use PhpTek\JSONText\ORM\FieldType\JSONText;
 
 class Bug extends DataObject
 {
@@ -19,7 +19,7 @@ class Bug extends DataObject
     private static $cascade_duplicates = [];
 
     private static $db = [
-        'Data' => DBJSONText::class,
+        'Data' => JSONText::class,
     ];
 
     private static $casting = [];
